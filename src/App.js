3e47515d45cf4413072be5ce2c3538c563eb57fb
Rohan Agarwal <arohan2907@gmail.com>
2021-06-06 22:47:30 +0530

    Added a navbar. Some colour adjustments.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import Results from "./components/Results";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <Results />;
+  return (
+    <div>
+      <Navbar />
+      <Results />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles = makeStyles((theme) => ({
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            F1 - Stats
+          </Typography>
+          {/* <Button color="inherit">Signup</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
